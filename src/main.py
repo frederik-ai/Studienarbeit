c324e3bc1a14b8b3930e0e2ff71b0204dba947b2
Frederik <frederikcpp@users.noreply.github.com>
2022-11-03 10:40:05 +0100

    Change folder structure

---
+++ src/main.py
@@ -0,0 +1,24 @@
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+
+# retrieve dataset
+# problem: data is not divided into classes
+# x_test = tf.keras.utils.image_dataset_from_directory('./data/Test')
+# process dataset
+# ...
+# initialize parameters
+# ...
+
+# region GAN D
+# ...
+# endregion
+
+# region GAN F
+# ...
+# endregion
+
+# define training functions
+# ...
+# execute training
+# ...
