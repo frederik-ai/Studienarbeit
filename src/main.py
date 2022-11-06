51def9035e4f4c63d575cf97b161f971a0a91bff
Frederik <frederikcpp@users.noreply.github.com>
2022-11-06 23:08:43 +0100

    Write first code for generator

--- src/main.py
+++ src/main.py
@@ -1,24 +1,46 @@
 import tensorflow as tf
 import os
 
+BATCH_SIZE = 32
+IMAGE_DIMENSIONS = (256, 256)
+
 # Retrieve Dataset
 training_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..',
                                               'data\\Official Train\\Final_Training\\Images'))
-x_train = tf.keras.utils.image_dataset_from_directory(training_path, batch_size=32)
 
-# Process Dataset
-# ...
+# x_train is a data.Dataset object
+x_train = tf.keras.utils.image_dataset_from_directory(training_path, batch_size=BATCH_SIZE, image_size=IMAGE_DIMENSIONS)
+x_train_shape = tf.shape(x_train)
 
-# Initialize Parameters
-# ...
 
-# region GAN D
-# ...
-# endregion
+# GAN
+# # Generator
+def make_generator_model():
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.InputLayer(input_shape=x_train_shape))
+    model.add(tf.keras.layers.Dense(32))
+    model.add(tf.keras.layers.BatchNormalization())  # add batch normalization before activation of input layer
+    model.add(tf.keras.layers.ReLU())
 
-# region GAN F
-# ...
-# endregion
+    model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), padding='same', activation='relu',
+                                     input_shape=x_train_shape))
+    # TODO: add fractional-strided convolution instead of pooling
+    model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), padding='same', activation='tanh',
+                                     input_shape=x_train_shape))
+    return model
+
+
+# # Discriminator
+def make_discriminator_model():
+    model = tf.keras.Sequential()
+    # TODO: add layers - use LeakyReLu in all layers
+
+    return model
+
+
+# %% Generate
+generator = make_generator_model()
+discriminator = make_discriminator_model()
 
 # Define Training Functions
 # ...
